f2d931179b1d292876bfd74bf3dc4c6a1258757e
XingKaiXin <xingkaixin@gmail.com>
2015-08-24 23:28:21 +0800

    Initial Commit

---
+++ PinPinAdmin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinPinAdmin
+//
+//  Created by 邢凯 on 15/8/24.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
